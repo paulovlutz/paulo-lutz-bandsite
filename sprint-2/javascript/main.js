d1858ae7ccdad0632ff674c71131e2b18b5fb205
Paulo <paulovlutz@gmail.com>
2020-05-02 12:37:31 -0400

    update comments section in the bio page for mobile breakpoint

--- sprint-2/javascript/main.js
+++ sprint-2/javascript/main.js
@@ -17,24 +17,49 @@ let conversationComments = [{
     },
 ];
 
-let commentsDiv = document.querySelector(".conversation__coments");
+let commentsDiv = document.querySelector(".conversation__comments");
 
 function displayComment(commentObj) {
 
     let comments__card = document.createElement("div");
     comments__card.className = "comments__card";
 
+    let comment__row = document.createElement("div");
+    comment__row.className = "row";
+
+    let column__picture = document.createElement("div");
+    column__picture.className = "column__picture";
+    comment__row.appendChild(column__picture);
+
+    let profile__picture = document.createElement("img");
+    profile__picture.src = "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+    profile__picture.className = "profile-picture";
+    column__picture.appendChild(profile__picture);
+
+    let column__details = document.createElement("div");
+    column__details.className = "column__details";
+    comment__row.appendChild(column__details);
+
+    let column__namedate = document.createElement("div");
+    column__namedate.className = "column__namedate";
+
     let comment__name = document.createElement("p");
     comment__name.innerText = commentObj["name"];
-    comments__card.appendChild(comment__name);
+    comment__name.className = "name__paragraph";
+    column__namedate.appendChild(comment__name);
 
     let comment__date = document.createElement("p");
+    comment__date.className = "date__paragraph"
     comment__date.innerText = commentObj["date"];
-    comments__card.appendChild(comment__date);
+    column__namedate.appendChild(comment__date);
+
+    column__details.append(column__namedate);
 
     let comment__text = document.createElement("p");
     comment__text.innerText = commentObj["comment"];
-    comments__card.appendChild(comment__text);
+    column__details.appendChild(comment__text);
+
+    comments__card.appendChild(comment__row);
     commentsDiv.appendChild(comments__card);
 }
 
@@ -42,11 +67,14 @@ for (let i = 0; i < conversationComments.length; i++) {
     displayComment(conversationComments[i]);
 }
 
+// Form
+
 let form = document.querySelector(".conversation__form");
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     let newCommentObj = {
         "name": e.target.name.value,
+        "date": "05/02/2020",
         "comment": e.target.comment.value
     }
 
