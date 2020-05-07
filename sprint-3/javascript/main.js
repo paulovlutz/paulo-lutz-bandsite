10c75724e358a5a8e0a3bca7227955c030570ffc
Paulo <paulovlutz@gmail.com>
2020-05-07 15:42:08 -0400

    add sprint-3 folder

---
+++ sprint-3/javascript/main.js
@@ -0,0 +1,92 @@
+let conversationComments = [{
+        "name": "Micheal Lyons",
+        "date": "12/18/2018",
+        "comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+    },
+
+    {
+        "name": "Gary Wong",
+        "date": "12/12/2018",
+        "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    },
+
+    {
+        "name": "Theodore Duncan",
+        "date": "11/15/2018",
+        "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+    },
+];
+
+let commentsDiv = document.querySelector(".conversation__comments");
+
+function displayComment(commentObj) {
+
+    let comments__card = document.createElement("div");
+    comments__card.className = "conversation__card";
+
+    let comment__row = document.createElement("div");
+    comment__row.className = "conversation__row";
+
+    let column__picture = document.createElement("div");
+    column__picture.className = "conversation__picture";
+    comment__row.appendChild(column__picture);
+
+    let profile__picture = document.createElement("img");
+    profile__picture.src = "./assets/images/profile-picture.jpeg";
+    profile__picture.className = "conversation__profile-picture";
+    column__picture.appendChild(profile__picture);
+
+    let column__details = document.createElement("div");
+    column__details.className = "conversation__details";
+    comment__row.appendChild(column__details);
+
+    let column__namedate = document.createElement("div");
+    column__namedate.className = "conversation__nameAndDate";
+
+    let comment__name = document.createElement("p");
+    comment__name.innerText = commentObj["name"];
+    comment__name.className = "conversation__nameAndDate-name";
+    column__namedate.appendChild(comment__name);
+
+    let comment__date = document.createElement("p");
+    comment__date.className = "conversation__nameAndDate-date"
+    comment__date.innerText = commentObj["date"];
+    column__namedate.appendChild(comment__date);
+
+    column__details.append(column__namedate);
+
+    let comment__text = document.createElement("p");
+    comment__text.innerText = commentObj["comment"];
+    column__details.appendChild(comment__text);
+
+    comments__card.appendChild(comment__row);
+    commentsDiv.appendChild(comments__card);
+}
+
+for (let i = 0; i < conversationComments.length; i++) {
+    displayComment(conversationComments[i]);
+}
+
+// Form
+
+let form = document.querySelector(".conversation__form");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let newDate = new Date();
+    let currentDate = (newDate.getMonth()+1)+'/'+newDate.getDate()+'/'+newDate.getFullYear();
+    let newCommentObj = {
+        "name": e.target.name.value,
+        "date": currentDate,
+        "comment": e.target.comment.value
+    }
+
+    conversationComments.unshift(newCommentObj);
+
+    commentsDiv.innerText = "";
+
+    for (let i = 0; i < conversationComments.length; i++) {
+        displayComment(conversationComments[i]);
+    }
+
+    form.reset();
+});
---
+++ sprint-3/javascript/shows.js
@@ -0,0 +1,100 @@
+let shows__array = [
+    {
+        "date": "Mon Dec 17 2018",
+        "venue": "Ronald Lane",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Tue Jul 18 2019",
+        "venue": "Pier 3 East",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Fri Jul 2019",
+        "venue": "View Loungue",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Sat Aug 12 2019",
+        "venue": "Hyatt Agency",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Fri Sep 05 2019",
+        "venue": "Moscow Center",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Wed Aug 11 2019",
+        "venue": "Pres Club",
+        "location": "San Francisco, CA"
+    }
+];
+
+let table = document.querySelector(".shows__table");
+
+let divTable = document.createElement("div");
+divTable.className = "tablet__header";
+
+let headerDates = document.createElement("p");
+headerDates.innerText = "DATES";
+headerDates.className = "tablet__head";
+let headerVenue = document.createElement("p");
+headerVenue.innerText = "VENUE";
+headerVenue.className = "tablet__head";
+let headerLocation = document.createElement("p");
+headerLocation.innerText = "LOCATION";
+headerLocation.className = "tablet__head";
+
+divTable.appendChild(headerDates);
+divTable.appendChild(headerVenue);
+divTable.appendChild(headerLocation);
+table.appendChild(divTable);
+
+function displayShow(showObj) {
+    let shows__card = document.createElement("div");
+    shows__card.className = "shows__card";
+
+    let shows__dateText = document.createElement("p");
+    shows__dateText.innerText = "DATE";
+    shows__dateText.className = "shows__heading";
+    shows__card.appendChild(shows__dateText);
+
+    let shows__date = document.createElement("h3");
+    shows__date.innerText = showObj["date"];
+    shows__card.appendChild(shows__date);
+
+    let shows__venueText = document.createElement("p");
+    shows__venueText.innerText = "VENUE";
+    shows__venueText.className = "shows__heading";
+    shows__card.appendChild(shows__venueText);
+
+    let shows__venue = document.createElement("p");
+    shows__venue.innerText = showObj["venue"];
+    shows__card.appendChild(shows__venue);
+
+    let shows__locationText = document.createElement("p");
+    shows__locationText.innerText = "LOCATION";
+    shows__locationText.className = "shows__heading";
+    shows__card.appendChild(shows__locationText);
+
+    let shows__location = document.createElement("p");
+    shows__location.innerText = showObj["location"];
+    shows__card.appendChild(shows__location);
+
+    document.querySelector(".shows__table").appendChild(shows__card);
+
+    let shows__button = document.createElement("button");
+    shows__button.innerText = "BUY TICKETS";
+    shows__button.classList.add("shows__button");
+    shows__card.appendChild(shows__button);
+}
+
+for (let i = 0; i < shows__array.length; i++) {
+    displayShow(shows__array[i]);
+}
